65f193ef5f8a6b1b2c254d08da9c00f216b8aaa8
shiraishi <baishizhenling@gmail.com>
2021-11-08 14:28:59 +0900

    コメント追加

--- TestProject/src/testPackage/TestClass2.java
+++ TestProject/src/testPackage/TestClass2.java
@@ -1,7 +1,34 @@
 package testPackage;
 
+//------------------------------------------------------------------------------
+/**
+ * comment<br><br> 
+ *
+ * <p>クラス名称： title</p>
+ *
+ * <p>著作権： Copyright (c) 2006 EnterSystem CO.,LTD. All Rights Reserved.</p>
+ *
+ * <p>更新履歴：
+ * <pre>
+ *     VerNo.       author           update     comment
+ *     version      user             2021/11/08 comment
+ * </pre>
+ * </p>
+ * @author user
+ * @version version
+ */
+//------------------------------------------------------------------------------
 public class TestClass2 {
 
+	//--------------------------------------------------------------------------
+	/**
+	 * comment<br><br>
+	 *
+	 * <p>メソッド名称： title</p>
+	 *
+	 * @param args
+	 */
+	//--------------------------------------------------------------------------
 	public static void main(String[] args) {
 		System.out.println( "Test");
 
