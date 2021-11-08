0dc2b7cfeba9dfd05fdcea4f37a73dba28af5a7b
shiraishi <baishizhenling@gmail.com>
2021-11-08 15:41:52 +0900

    文字化け検証

--- TestProject/src/testPackage/TestClass2.java
+++ TestProject/src/testPackage/TestClass2.java
@@ -2,18 +2,18 @@ package testPackage;
 
 //------------------------------------------------------------------------------
 /**
- * comment<br><br> 
- *
- * <p>繧ｯ繝ｩ繧ｹ蜷咲ｧｰ�ｼ� title</p>
- *
- * <p>闡嶺ｽ懈ｨｩ�ｼ� Copyright (c) 2006 EnterSystem CO.,LTD. All Rights Reserved.</p>
- *
- * <p>譖ｴ譁ｰ螻･豁ｴ�ｼ�
- * <pre>
- *     VerNo.       author           update     comment
- *     version      user             2021/11/08 comment
- * </pre>
- * </p>
+* comment<br><br> 
+*
+* <p>クラス名称： title</p>
+*
+* <p>著作権： Copyright (c) 2006 EnterSystem CO.,LTD. All Rights Reserved.</p>
+*
+* <p>更新履歴：
+* <pre>
+*     VerNo.       author           update     comment
+*     version      user             2021/11/08 comment
+* </pre>
+* </p>
  * @author user
  * @version version 00.00.01
  */
