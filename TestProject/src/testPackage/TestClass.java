6088a2cc2752193809cc278c4f030ef154fdafbb
baishizhenling <93891129+baishizhenling@users.noreply.github.com>
2021-11-08 14:14:04 +0900

    Update TestClass.java

--- TestProject/src/testPackage/TestClass.java
+++ TestProject/src/testPackage/TestClass.java
@@ -4,11 +4,11 @@ package testPackage;
 /**
  * comment<br><br> 
  *
- * <p>ƒNƒ‰ƒX–¼ÌF title</p>
+ * <p>ã‚¯ãƒ©ã‚¹åç§°ï¼š title</p>
  *
- * <p>’˜ìŒ F Copyright (c) 2006 EnterSystem CO.,LTD. All Rights Reserved.</p>
+ * <p>è‘—ä½œæ¨©ï¼š Copyright (c) 2006 EnterSystem CO.,LTD. All Rights Reserved.</p>
  *
- * <p>XV—š—ğF
+ * <p>æ›´æ–°å±¥æ­´ï¼š
  * <pre>
  *     VerNo.       author           update     comment
  *     version      user             2021/11/08 comment
@@ -24,13 +24,15 @@ public class TestClass {
 	/**
 	 * comment<br><br>
 	 *
-	 * <p>ƒƒ\ƒbƒh–¼ÌF title</p>
+	 * <p>ãƒ¡ã‚½ãƒƒãƒ‰åç§°ï¼š title</p>
 	 *
 	 * @param args
 	 */
 	//--------------------------------------------------------------------------
 	public static void main(String[] args) {
 		System.out.println( "Test");
+		System.out.println( "Test");
+
 	}
 
 }
